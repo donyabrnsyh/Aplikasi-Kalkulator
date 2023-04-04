dce4af7bd8d1b2077e579d1f05334ea56ae52c0a
donyabrnsyh <dony.abriansyah72@gmail.com>
2023-04-04 22:58:58 +0700

    update

---
+++ script.js
@@ -0,0 +1,15 @@
+function insert(parameter) {
+  call.layar.value += parameter;
+}
+
+function ac() {
+  call.layar.value = "";
+}
+
+function equal() {
+  call.layar.value = eval(call.layar.value);
+
+  // if (eval(call.layar.value) === parseFloat) {
+  //   return parsefloat(call.layar.valeu);
+  //   }
+}
